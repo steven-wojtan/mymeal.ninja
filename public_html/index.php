bf46affbe1b64cbd7e432354b9e4ed40d0dd5a16
Steven Wojtan <steven.wojtan.15@cnu.edu>
2019-02-12 21:51:19 -0500

    no comment

--- public_html/index.php
+++ public_html/index.php
@@ -14,10 +14,10 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Meal planning website">
+    <meta name="author" content="Steven Wojtan">
 
-    <title>Meal Ninja - Contact</title>
+    <title>Meal Ninja</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -26,28 +26,28 @@
 
 <body>
 
-<!-- Navigation -->
+<!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
     <div class="container">
-        <a class="navbar-brand" href="#">Start Bootstrap</a>
+        <a class="navbar-brand" href="#">My Meal Ninja</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home
+                    <a class="nav-link" href="/">Home
                         <span class="sr-only">(current)</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Plan</a>
+                    <a class="nav-link" href="/plan">Plan</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Tools</a>
+                    <a class="nav-link" href="/tools">Tools</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
+                    <a class="nav-link" href="/about">About</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/contact">Contact</a>
@@ -56,6 +56,8 @@
         </div>
     </div>
 </nav>
+
+
 
 <!-- Page Content -->
 <div class="container">
